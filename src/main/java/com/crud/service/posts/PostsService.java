8c8b5eafbe6a006fff626721a757641000840eac
brorica <minjea917@gmail.com>
2022-12-21 02:23:04 +0900

    4장 따라치기

--- src/main/java/com/crud/service/posts/PostsService.java
+++ src/main/java/com/crud/service/posts/PostsService.java
@@ -2,9 +2,12 @@ package com.crud.service.posts;
 
 import com.crud.domain.posts.Posts;
 import com.crud.domain.posts.PostsRepository;
+import com.crud.web.dto.PostsListResponseDto;
 import com.crud.web.dto.PostsResponseDto;
 import com.crud.web.dto.PostsSaveRequestDto;
 import com.crud.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,5 +37,19 @@ public class PostsService {
             .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }

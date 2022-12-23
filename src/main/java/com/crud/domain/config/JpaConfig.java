1ddc728084d6e8abc466e89869bae4ea9adfa963
brorica <minjea917@gmail.com>
2022-12-23 18:31:05 +0900

    5장 따라치기 완료

---
+++ src/main/java/com/crud/domain/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.crud.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}

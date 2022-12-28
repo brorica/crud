e61b9048c5c7c0aec85b984a915b625e657a835b
brorica <minjea917@gmail.com>
2022-12-28 19:11:11 +0900

    nginx 스크립트 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,7 @@ PROJECT_NAME=crud
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
@@ -17,6 +18,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
+
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
